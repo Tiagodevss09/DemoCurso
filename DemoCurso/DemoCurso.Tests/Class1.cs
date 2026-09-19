b89c7f99e0d13dcda1458075d9195399dbe1c6b9
Tiagodev_ss <Tiagodev_ss@hotmail.com>
2026-09-19 19:10:40 -0300

    enviando imagem para docker hub

---
+++ DemoCurso/DemoCurso.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace DemoCurso.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Somar_DeveRetornarResultadoCorreto()
+        {
+            // Arrange
+            var resultado = 2 + 3;
+
+            // Act & Assert
+            Assert.Equal(5, resultado);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DemoCurso
+
 DemoCurso
+
+
+
+\[!\[Continuous Integration, Delivery and Deployment with Github Actions and .NET 10](https://github.com/Tiagodevss09/DemoCurso/actions/workflows/docker-image.yml/badge.svg)](https://github.com/Tiagodevss09/DemoCurso/actions/workflows/docker-image.yml)
+
